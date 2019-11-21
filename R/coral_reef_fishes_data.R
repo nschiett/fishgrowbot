3416ce58fb4047501ca345d225194fdfbcd6a58c
nschiett <nina.schiettekatte@gmail.com>
2019-11-21 19:41:48 +0100

    first commit

---
+++ R/coral_reef_fishes_data.R
@@ -0,0 +1,28 @@
+#' coral_reef_fishes_data
+#'
+#'
+#' @details
+#' This is the dataset provided by **add datapaper citation **
+#' It contains the following data:
+#' family: Family
+#' genus: Genus
+#' species: Species
+#' id: Unique code identifying each individual
+#' agei: Age i (years)
+#' radi: Otolith radius at age i (mm)
+#' agecap: Age of the fish at capture (years)
+#' radcap: Radius of the otolith at capture (mm)
+#' lencap: Length of the fish at capture (total length, mm)
+#' l0p: Length of the fish at hatching (mm)
+#' biomass: Wet body mass of the fish at capture (g)
+#' location: Island of the sampling
+#'
+#' @docType data
+#'
+#' @usage data(coral_reef_fishes_data)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(coral_reef_fishes_data)
+"coral_reef_fishes_data"
---
+++ R/fishgrowbot-package.R
@@ -0,0 +1,16 @@
+#' The 'fishgrowbot' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name fishgrowbot-package
+#' @aliases fishgrowbot
+#' @useDynLib fishgrowbot, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#'
+NULL
